cd561ba65cea03d0d26caede372990290705ec53
nadja <you@example.com>
2021-12-03 15:03:42 +0000

    created additonal const and data outputs

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-const h2 = document.querySelector("h2");
+const recipeName = document.querySelector("h3");
+const servingSize = document.querySelector(".serving-size");
+const recipeImage = document.querySelector("#recipe-Image");
+const mealType = document.querySelector(".meal-type");
+const time = document.querySelector(".time");
 
 function handleRecipeClick() {}
 
@@ -8,17 +12,22 @@ function handleInputChange() {}
 
 
 async function fetchRecipe() {
-    const respone = await fetch("https://api.edamam.com/api/recipes/v2?type=public&q=potato&app_id=740fa198&app_key=de153b0e2a49cf4512763c34501b10f1&imageSize=LARGE&random=true");
+    const respone = await fetch("https://api.edamam.com/api/recipes/v2?type=public&q=potato&app_id=740fa198&app_key=de153b0e2a49cf4512763c34501b10f1&time=20-60&imageSize=REGULAR");
     const data = await respone.json();
     console.log(data);
     const recipe = data.hits[0]["recipe"];
+    console.log(recipe);
     renderData(recipe);
     
 
     }
 
 function renderData(recipe) {
-    h2.innerText = recipe.label;
+    recipeName.innerText = recipe.label;
+    servingSize.innerText = recipe.yield
+    recipeImage.src = recipe.image;
+    mealType.innerText = recipe.mealType;
+    time.innerText = recipe.totalTime;
 }    
 
 
